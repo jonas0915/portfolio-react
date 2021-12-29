280845d1f7396f6fb37464d1fefeedcda6c1b6f6
Jonas Penaso <edciljonas15@yahoo.com>
2021-12-29 13:17:50 -0800

    add files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import { AppsSharp } from "@material-ui/icons";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const port = process.env.PORT || 3000;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
-App.listen(port, function () {
-  console.log("Server has started successfully");
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log("Server is running on port", port);
 });
