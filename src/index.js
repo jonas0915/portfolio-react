ac06328098d681a270645b27dca55a923f569c05
Jonas Penaso <edciljonas15@yahoo.com>
2021-12-28 16:57:07 -0800

    add stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import { AppsSharp } from "@material-ui/icons";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+App.listen(port, function () {
+  console.log("Server has started successfully");
+});
