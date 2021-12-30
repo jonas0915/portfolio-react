86ff4e9c29b4d2561d75195ffaed4b63d3c52473
Jonas Penaso <edciljonas15@yahoo.com>
2021-12-29 21:21:00 -0800

    make it better

---
+++ build/precache-manifest.69aa963b124a0f1ecc2f42287bc9bc82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29c32f4f96c49adbf21c1405f714188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47106b816887bd135c35",
+    "url": "/static/js/2.55c462c9.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.55c462c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748206e5c0cf883b33bc",
+    "url": "/static/js/main.f36ee2d5.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "720da348d0d5d34f818f3a2acdbdae92",
+    "url": "/static/media/Prof.720da348.jpg"
+  },
+  {
+    "revision": "ca5ae92800b57f4c2c3cbc42a65d866b",
+    "url": "/static/media/Project1.ca5ae928.png"
+  },
+  {
+    "revision": "e2cdfdb9b633b4477f21032c672cc6cf",
+    "url": "/static/media/Project2.e2cdfdb9.png"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-const express = require("express");
-const app = express();
-
-const path = require("path");
-const port = process.env.PORT || 3000;
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("/", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
-
-app.listen(port, (err) => {
-  if (err) return console.log(err);
-  console.log("Server is running on port", port);
-});
