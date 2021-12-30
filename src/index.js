89869014312a666035f04da652bd466e6f1ceef6
Jonas Penaso <edciljonas15@yahoo.com>
2021-12-29 17:44:59 -0800

    5th commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     req.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
