114f370b0349d5b8bfd8a005790182eb021c50bc
Jonas Penaso <edciljonas15@yahoo.com>
2022-08-03 20:43:47 -0700

    profile update

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -10,16 +10,16 @@ function Introduction() {
         </div>
         <div>
           <p>
-            - I am a Full Stack Web Develper with experience in <space></space>
+            - I'm a self taught Web Develper with experience in <space></space>
             <span className="language">
-              HTML & CSS, JavaScript, React, NodeJS, Express, SQL, Mongoose,
-              MongoDB, Git, and Github. <br />
+              HTML & CSS, JavaScript, React, SQL, MongoDB, Git, and Github.
+              <br />
             </span>
-            My background is in the Culinary Industry working with teams in a
+            My background is in the Culinary Industry, working with teams in a
             fast-paced high quality work environment.
             <br />
-            Hobbies I really enjoy includes working out, trying out new and good
-            places to eat around the city, and of course travelling..
+            Hobbies I enjoy includes working out, trying out new and good places
+            to eat around the city, and of course travelling..
           </p>
         </div>
       </div>
