b89e7620573325aceefce3058f35fe8ba6376b11
Jonas Penaso <edciljonas15@yahoo.com>
2022-08-03 22:01:32 -0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require("path");
 
-const port = process.env.PORT || 50;
+const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
